cd6d86bb04b6c3d3e9f8b3b40667c5a53241800a
NEHA GOLE <nehaagole@gmail.com>
2025-01-08 23:01:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-document.getElementById('menu-icon').addEventListener('click', function() {
-    const navbar = document.querySelector('.navbar');
-    navbar.style.display = navbar.style.display === 'flex' ? 'none' : 'flex';
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('menu-icon').addEventListener('click', function() {
+        const navbar = document.querySelector('.navbar');
+        navbar.style.display = navbar.style.display === 'flex' ? 'none' : 'flex';
+    });
 });
+
